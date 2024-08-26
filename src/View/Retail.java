ae0c4ea1bc7656a5d1493aaab713980bc2b003eb
JFBP <jai.barraza@duocuc.cl>
2024-08-25 23:54:19 -0400

    Proyecto de Tienda Online de ropa, utilizando patrones de diseño, bajo el concepto de Modelo-Vista-Controlador (MVC). v4

--- src/View/Retail.java
+++ src/View/Retail.java
@@ -2,15 +2,9 @@ package View;
 
 import Controller.RopaComponenteConcreto;
 
-
 public class Retail {
-
-    
     public static void main(String[] args) {
-       // View.Menu_principal();
         RopaComponenteConcreto ropaObjeto = new RopaComponenteConcreto(); // Los Decoradores se irán activando (O no) en el transcurso de la venta.
-        ropaObjeto.vender();
-        
-    }
-    
+        ropaObjeto.vender();        
+    }  
 }
